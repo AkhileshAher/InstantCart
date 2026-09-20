394499f877f6ac343c1ebd69acd9f181fb7a3cea
AkhileshAher <aherakhilesh88@gmail.com>
2026-09-20 10:31:44 +0530

    Mongo DB Config Fix and Changes in Docker Compose

--- InstantCart-backend/src/main/java/in/akhilesh/instantcart/config/MongoConfig.java
+++ InstantCart-backend/src/main/java/in/akhilesh/instantcart/config/MongoConfig.java
@@ -6,17 +6,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.data.mongodb.core.MongoTemplate;
 
 @Configuration
 @EnableMongoAuditing
 public class MongoConfig {
 
-    @Value("${spring.data.mongodb.uri}")
+    @Value("${DB_URL}")
     private String mongoUri;
 
     @Bean
     public MongoClient mongoClient() {
         return MongoClients.create(mongoUri);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "instantcart");
     }
 
 }
